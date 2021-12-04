2666b21f74954b7ad32d8c420019a3b794922509
MJ <mdjenkins655@gmail.com>
2021-12-04 10:01:21 -0500

    Adding routes and a model

---
+++ Develop/models/workout.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  exercises:[ 
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter a type of exercise"
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter a name for exercise"
+      },
+      duration: {
+        type: Number,
+        required: "Enter a duration"
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number, 
+      },
+      distance: {
+        type: Number,
+      }
+
+    }
+  ],
+  day: {
+    type: Date,
+    default: Date.now
+  }
+  
+
+
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+//Why isn't this green
+module.exports = Workout;
---
+++ Develop/routes/api.js
@@ -0,0 +1,86 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+//Copied over, don't know if needed
+router.post("/workout/bulk", ({ body }, res) => {
+  Workout.insertMany(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+//Was /api/workout
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+//Exercise GET Route - Placeholder
+router.get("/exercise", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+//Stats GET Route - Place Holder
+router.get("/stats", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+//Add get workouts/range
+router.get("/api/workouts/range", (req, res) => {
+  
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+//Add put route for new exercise
+//Find by ID
+//Push to the array
+router.put("/api/workouts/:id", ({ body }, res) => {
+  Workout.insertMany(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+module.exports = router;
