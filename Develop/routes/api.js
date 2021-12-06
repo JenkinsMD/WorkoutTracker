3735e709879fa34fbb287cfebd574d0f3c3b0cc5
MJ <mdjenkins655@gmail.com>
2021-12-06 02:04:16 -0500

    updated routes

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { db } = require("../models/workout.js");
 const Workout = require("../models/workout.js");
 
 router.post("/api/workouts", ({ body }, res) => {
@@ -11,76 +12,91 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
-//Copied over, don't know if needed
-router.post("/workout/bulk", ({ body }, res) => {
-  Workout.insertMany(body)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
-
 //Was /api/workout
 router.get("/api/workouts", (req, res) => {
-  Workout.find({})
-    .sort({ date: -1 })
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+
+    Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {$sum: "$exercises.duration"}
+        }
+      }
+    ]
+  )
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
 });
 
 //Exercise GET Route - Placeholder
-router.get("/exercise", (req, res) => {
-  Workout.find({})
-    .sort({ date: -1 })
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
+// router.get("/exercise", (req, res) => {
+//   Workout.find({})
+//     .sort({ date: -1 })
+//     .then(dbWorkout => {
+//       res.json(dbWorkout);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
 
 //Stats GET Route - Place Holder
-router.get("/stats", (req, res) => {
-  Workout.find({})
-    .sort({ date: -1 })
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
+// router.get("/stats", (req, res) => {
+//   Workout.find({})
+//     .sort({ date: -1 })
+//     .then(dbWorkout => {
+//       res.json(dbWorkout);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
 
 //Add get workouts/range
 router.get("/api/workouts/range", (req, res) => {
+  // console.log(" range req ---" + JSON.stringify(req))
   
-  Workout.find({})
-    .sort({ date: -1 })
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+  //add field toal duration
+  Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {$sum: "$exercises.duration"}
+        }
+      }
+    ]
+  )
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+
+
+  
+
 });
 
 //Add put route for new exercise
 //Find by ID
 //Push to the array
-router.put("/api/workouts/:id", ({ body }, res) => {
-  Workout.insertMany(body)
+router.put("/api/workouts/:id", (req, res) => {
+  console.log("req.params---"+ JSON.stringify(req.params.id))
+  console.log("req.body---"+ JSON.stringify(req.body))
+  Workout.updateOne({"_id": req.params.id}, {$push:{exercises: req.body}})
     .then(dbWorkout => {
       res.json(dbWorkout);
+  
     })
     .catch(err => {
       res.status(400).json(err);
     });
+
+  
 });
 module.exports = router;
--- Develop/routes/view.js
+++ Develop/routes/view.js
@@ -9,6 +9,9 @@ router.get('/stats', (req, res) =>{
     res.sendFile(path.join(__dirname,"../public/stats.html"))
 });
 
+router.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname,"../public/index.html"))
+});
 
 module.exports = router;
 
